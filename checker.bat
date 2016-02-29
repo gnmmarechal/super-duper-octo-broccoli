c99acd46b89ebb805cac0a8fed5b574a6bcfc530
Mario Alexandre Lopes Liberato <gnmpolicemata91@gmail.com>
2016-02-29 22:04:24 +0000

    Second first commit, I suppose

---
+++ checker.bat
@@ -0,0 +1,11 @@
+@echo off
+::CIA Updater Pack Checker Script
+::By gnmpolicemata/gnmmarechal 
+:: http://reddit.com/u/3dshacks
+:: http://youtube.com/gnmpolicemata
+
+::Version
+set major = 1
+set minor = 0
+set rev = 0
+
